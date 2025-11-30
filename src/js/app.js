ddacfb053be27c608107ef2220754a5060ab15c3
Daniel Kowalczyk <kostex2@gmail.com>
2025-11-30 15:11:33 +0000

    Implement Booking class with people and hours

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,61 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
-  import {settings, select} from './settings.js';
+  import {settings, select, classNames} from './settings.js';
   import Product from './components/Product.js';
   import Cart from './components/Cart.js';
+  import Booking from './components/Booking.js';
 
   const app = {
+    initPages: function(){
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const idFromHash = window.location.hash.replace('#/', '');
+
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id == idFromHash){
+          pageMatchingHash = page.id;
+          break;
+        }
+      }
+
+      thisApp.activatePage(pageMatchingHash);
+
+      for(let link of thisApp.navLinks){
+        link.addEventListener('click', function(event){
+          const clickedElement = this;
+          event.preventDefault();
+          /* get page id from href attribute*/
+          const id = clickedElement.getAttribute('href').replace('#', '');
+          /* run thisApp.activatePage with that id*/
+          thisApp.activatePage(id);
+          /* change URL hash */
+          window.location.hash = '#/' + id;
+        });
+      }
+    },
+
+    activatePage: function(pageId){
+      const thisApp = this;
+
+      /* add class "active" to matching pages, remove fron non-matching*/
+      for(let page of thisApp.pages){
+        page.classList.toggle(classNames.pages.active, page.id == pageId);
+      }
+
+       /* add class "active" to matching links, remove fron non-matching*/
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active,
+          link.getAttribute('href') == '#' + pageId
+        );
+      }
+
+    },
     initData: function(){
       const thisApp = this;
 
@@ -16,7 +67,6 @@
           return rawResponse.json();
         })
         .then(function(parsedResponse){
-          console.log('parsedResponse:', parsedResponse);
 
           thisApp.data.products = parsedResponse;
 
@@ -27,8 +77,6 @@
     initMenu: function(){
       const thisApp = this;
       
-
-
       for(let productData in thisApp.data.products){
         new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
       }
@@ -37,8 +85,10 @@
     init: function(){
       const thisApp = this;
 
+      thisApp.initPages();
       thisApp.initData();
       thisApp.initCart();
+      thisApp.initBooking();
     },
 
     initCart: function(){
@@ -52,6 +102,12 @@
       thisApp.productList.addEventListener('add-to-cart', function(event){
         thisApp.cart.add(event.detail.product);
       })
+    },
+    initBooking: function(){
+      const thisApp = this;
+
+      const bookingElem = document.querySelector(select.containerOf.booking);
+      thisApp.booking = new Booking(bookingElem);
     },
   };
   
